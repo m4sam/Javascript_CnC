f86da211d311b1453f8e8cfd42c56a9b68a06352
Manav <128348285+m4sam@users.noreply.github.com>
2023-09-06 09:06:12 +0000

    datatypes Summamry

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -51,3 +51,26 @@ console.log(typeof anotherId);
 */
 // https://262.ecma-international.org/5.1/#sec-11.4.3
 // JavaScript is a dynamically typed language. Because data type will automatically assigned at the time of compilation or code execution
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive) , Heap (Non-Primitive)
+let email = "manav@google.com"
+let myName = "Manav"
+let anotherName = myName
+anotherName = "Sam"
+
+console.log(myName)
+console.log(anotherName)
+
+let userOne = {
+    email : "manav@google.com",
+    upi : "manav@apl"
+}
+
+let userTwo = userOne
+userTwo.email = "sam@google.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)
