b53048e77b5d6120376581d6aa14f87690465aeb
Manav <128348285+m4sam@users.noreply.github.com>
2023-09-09 18:53:58 +0000

    strings

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -3,33 +3,34 @@ const repoCount = 50
 
 // console.log(name + repoCount + " Value");
 
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); // prefered way
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); 
+// prefered way
 
-const gameName = new String('manav-sam-com')
+const gameName = new String('manavsamcom')
 
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
 
 
-// console.log(gameName.length);
-// console.log(gameName.toUpperCase());
-console.log(gameName.charAt(2));
-console.log(gameName.indexOf('t'));
+// // console.log(gameName.length);
+// // console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('n'));
 
-const newString = gameName.substring(0, 4)
-console.log(newString);
+// const newString = gameName.substring(0, 4)
+// console.log(newString);
 
-const anotherString = gameName.slice(-8, 4)
+const anotherString = gameName.slice(-6, 8)
 console.log(anotherString);
 
-const newStringOne = "   manav    "
-console.log(newStringOne);
-console.log(newStringOne.trim());
+// const newStringOne = "   manav    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
 
-const url = "https://hitesh.com/hitesh%20choudhary"
+const url = "https://manav.com/manav%20sam"
 
-console.log(url.replace('%20', '-'))
+// console.log(url.replace('%20', '-'))
 
-console.log(url.includes('sundar'))
+// console.log(url.includes('sundar')) // if exists T/F
 
-console.log(gameName.split('-'));
+// console.log(gameName.split('-'));
